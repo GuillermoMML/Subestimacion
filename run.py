a0bfe0ee3b773a27e5f1fe78fcdf8c7efe6f1503
guill <guillermoml2002@gmail.com>
2022-11-15 10:07:15 +0100

    Subestimacion v2

--- run.py
+++ run.py
@@ -9,15 +9,17 @@ lu = search.GPSProblem('L', 'U', search.romania)
 print(search.breadth_first_graph_search(ab).path())
 print(search.depth_first_graph_search(ab).path())
 
-print("Subestimación")
+print("Pruebas de Busqueda de  Ramificación y acotación sin Subestimación")
 print(search.ramificacion_y_acotacion(ab).path())
+print("Pruebas de Busqueda de  Ramificación y acotación con Subestimación")
 print(search.ramificacion_y_acotacion_con_subestimacion(ab).path())
-
-
+print("Pruebas de Busqueda de  Ramificación y acotación sin Subestimación")
 print(search.ramificacion_y_acotacion(op).path())
+print("Pruebas de Busqueda de  Ramificación y acotación con Subestimación")
 print(search.ramificacion_y_acotacion_con_subestimacion(op).path())
-
+print("Pruebas de Busqueda de  Ramificación y acotación sin Subestimación")
 print(search.ramificacion_y_acotacion(lu).path())
+print("Pruebas de Busqueda de  Ramificación y acotación con Subestimación")
 print(search.ramificacion_y_acotacion_con_subestimacion(lu).path())
 
 
